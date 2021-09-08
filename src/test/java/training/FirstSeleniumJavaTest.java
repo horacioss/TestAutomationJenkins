33f27e2940f84d050ba144cf8e40da94878c25ea
Horacio Macias <horacioss2013@gmail.com>
2021-09-08 13:49:24 -0500

    remove -> window().maximize()

--- src/test/java/training/FirstSeleniumJavaTest.java
+++ src/test/java/training/FirstSeleniumJavaTest.java
@@ -10,21 +10,19 @@ import org.testng.annotations.Test;
 
 public class FirstSeleniumJavaTest {
     private WebDriver driver;
-    private ChromeOptions options;
 
     @Test
     public void testEasy() {
         driver.get("https://testautomationu.applitools.com/");
-        driver.manage().window().maximize();
 
         String title = driver.getTitle();
-        AssertJUnit.assertTrue(title.equals("Test Automation University | Applitools"));
+        AssertJUnit.assertEquals(title ,"Test Automation University | Applitools");
     }
 
     @BeforeTest
     public void beforeTest() {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver93");
-        options = new ChromeOptions();
+        ChromeOptions options = new ChromeOptions();
         options.addArguments("headless");
         driver = new ChromeDriver(options);
     }
